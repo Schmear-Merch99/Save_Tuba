81f39bd84f9f10c9fd680fd2d0c368e17520a0d9
Schmear-Merch99 <srm222@lehigh.edu>
2020-07-21 20:55:57 -0400

    Save Tuba Project

--- app/src/main/java/com/example/save_tuba/MainActivity.java
+++ app/src/main/java/com/example/save_tuba/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    /*public void enter(View view){
 
+    }*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
